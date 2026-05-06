ebb49255e827e7c72044172b1e57c5f5294821ba
fomedemotivacao <fomedemotivacao@gmail.com>
2026-05-06 20:12:37 -0300

    Add Facebook Pixel via vite-plugin-html-inject

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import htmlInject from "vite-plugin-html-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: ":",
     port: 8080,
     hmr: {
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger(), htmlInject({
+    head: [`<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '1460767461849584'); fbq('track', 'PageView');
+</script><noscript> <img height="1" width="1" src="https://www.facebook.com/tr?id=1460767461849584&ev=PageView&noscript=1"/></noscript>
+<!-- End Facebook Pixel Code -->`]
+  })].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
