28cdb710f4de0cf695c2e7f8cdec814191441718
fomedemotivacao <fomedemotivacao@gmail.com>
2026-05-06 20:35:45 -0300

    Add Meta Pixel via Vite transformIndexHtml hook

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import htmlInject from "vite-plugin-html-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,13 +12,19 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger(), htmlInject({
-    head: [`<!-- Facebook Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '1460767461849584'); fbq('track', 'PageView');
-</script><noscript> <img height="1" width="1" src="https://www.facebook.com/tr?id=1460767461849584&ev=PageView&noscript=1"/></noscript>
-<!-- End Facebook Pixel Code -->`]
-  })].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+    {
+      name: "meta-pixel",
+      transformIndexHtml(html: string) {
+        return html.replace(
+          "</head>",
+          `<!-- Meta Pixel Code -->\n<script>\n!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');\nfbq('init', '1460767461849584');\nfbq('track', 'PageView');\n</script>\n<noscript><img height="1" width="1" style="display:none"\nsrc="https://www.facebook.com/tr?id=1460767461849584&ev=PageView&noscript=1"\n/></noscript>\n<!-- End Meta Pixel Code -->\n</head>`
+        );
+      },
+    },
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
