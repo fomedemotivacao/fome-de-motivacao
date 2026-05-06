20b13ca4da9d1e425f515bb750e94f83b35d90fe
fomedemotivacao <fomedemotivacao@gmail.com>
2026-05-06 20:20:49 -0300

    Revert FacebookPixel from App.tsx - fix blank page

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,12 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
-import { FacebookPixel } from "@/components/FacebookPixel";
-
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-        <FacebookPixel />
       <Sonner />
       <BrowserRouter>
         <Routes>
