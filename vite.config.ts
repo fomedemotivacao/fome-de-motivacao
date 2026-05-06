f4a6eb57eb5b56f1d070339e953b8b6d811e1f4c
fomedemotivacao <fomedemotivacao@gmail.com>
2026-05-06 20:37:04 -0300

    Revert vite.config.ts - transformIndexHtml caused blank page

--- vite.config.ts
+++ vite.config.ts
@@ -15,15 +15,6 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === "development" && componentTagger(),
-    {
-      name: "meta-pixel",
-      transformIndexHtml(html: string) {
-        return html.replace(
-          "</head>",
-          `<!-- Meta Pixel Code -->\n<script>\n!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');\nfbq('init', '1460767461849584');\nfbq('track', 'PageView');\n</script>\n<noscript><img height="1" width="1" style="display:none"\nsrc="https://www.facebook.com/tr?id=1460767461849584&ev=PageView&noscript=1"\n/></noscript>\n<!-- End Meta Pixel Code -->\n</head>`
-        );
-      },
-    },
   ].filter(Boolean),
   resolve: {
     alias: {
